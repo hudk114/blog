79f75adb646420b47dfe601ae6b0dc02c1afa599
hudk@win <523670182@qq.com>
2022-02-12 21:41:29 +0800

    feat: use vue-press

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Autor: alexdkhu
+ * @Date: 2022-02-12 21:26:08
+ * @LastEditors: alexdkhu
+ * @LastEditTime: 2022-02-12 21:29:26
+ */
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: '/blog/',
+}
